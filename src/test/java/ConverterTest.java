7dc16eb325a48d17cfdd17f46bb6d8980706c0c9
antonlai <anton.laine@metropolia.fi>
2024-02-27 15:19:03 +0200

    initial

---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,20 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConverterTest {
+
+    @Test
+    void fahrenheitToCelsius() {
+        double result = Converter.fahrenheitToCelsius(100);
+        assertEquals(37.7, result, 0.1);
+    }
+
+    @Test
+    void celsiusToFahrenheit() {
+        double result = Converter.celsiusToFahrenheit(37);
+        assertEquals(98.6, result, 0.001);
+    }
+}
